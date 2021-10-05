322e3c5ef6ef05e4a9eb0ef4d86bd6f9181e2986
paragkoche <paragkoche0@gmail.com>
2021-10-05 15:48:29 +0530

    test block chine

--- backend/src/block-chain/test.py
+++ backend/src/block-chain/test.py
@@ -1,3 +1,4 @@
+import json
 from os import write
 import time
 import hashlib as h
@@ -28,6 +29,18 @@ class BlockChain:
         previousHash = self.chain[len(
             self.chain)-1]['blockhash'] if len(self.chain) != 0 else ''
         Block = block(previoushash=previousHash, data=data)
-        Block.blockhash = Block.hase()
         self.chain.append({'id': Block.id, 'timestamp': Block.timestamp,
                           'blockhash': Block.blockhash, 'prevhash': Block.prevhash, 'data': Block.data})
+
+# code run 👆🏻👆🏻
+# cc = BlockChain()
+# cc.addBlocke({"ok": "ok"})
+# cc.addBlocke({"ok": "ok"})
+# cc.addBlocke({"ok": "ok"})
+# cc.addBlocke({"ok": "ok"})
+# cc.addBlocke({"ok": "ok"})
+# cc.addBlocke({"ok": "ok"})
+# print(cc.chain)
+# with open(r'D:\collage-project\backend\src\block-chain\chine.json', "w") as i:
+#     i.write(json.dumps(cc.chain))
+#     i.close()
